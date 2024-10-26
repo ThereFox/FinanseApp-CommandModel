6b91d322dbe97e686a4d2ca5f948462ab149728e
LAPTOP-RVPSKB3L\Elcu <alexandrgrizunow@gmail.com>
2024-10-26 19:27:35 +0400

    Add layer connection and configuration

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,10 @@
+using App;
+using Persistense;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder
+    .Configuration.GetValue<string>("ConnectionString");
 
 // Add services to the container.
 
@@ -6,6 +12,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddAppService()
+    .AddPersistense(connectionString);
 
 var app = builder.Build();
 
